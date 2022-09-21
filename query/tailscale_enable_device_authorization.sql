f18b7ecbae453ed420acf7efb1627f5de2e6e19e
niharika2k00 <niharika@turbot.com>
2022-09-21 13:41:21 +0800

    Modify

--- query/tailscale_enable_device_authorization.sql
+++ query/tailscale_enable_device_authorization.sql
@@ -4,6 +4,7 @@ select
   case
     when authorized = true then 'ok'
     else 'alarm'
+  end as status,
   case
     when authorized = true then 'Device is authorized.'
     else 'Device is not authorized.'
